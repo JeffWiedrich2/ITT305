1035d155f4c9c0f5b9e9a57eb3b9e417aeb3ca5d
JeffWiedrich2 <55073690+JeffWiedrich2@users.noreply.github.com>
2020-12-20 23:42:57 -0700

    Add files via upload

--- add_joke.php
+++ add_joke.php
@@ -8,7 +8,8 @@ ini_set('display_errors', 1);
 
 session_start();
 
-if(!$_SESSION['username'])
+
+if(!$_SESSION['userid'])
 {
 	echo "Only logged in user can access this page. Click <a href = 'login_form.php'> here </a> to login <br>";
 	exit;
@@ -24,16 +25,24 @@ $new_joke_question = addslashes($new_joke_question);
 $new_joke_answer = addslashes($new_joke_answer);
 $userid = $_SESSION['userid'];
 
-echo $new_joke_answer . "and" . $new_joke_question;
+echo "<h2> Trying to add a new joke ". $new_joke_question . " and ". $new_joke_answer."</h2>";
+$stmt = $mysqli->prepare("INSERT INTO jokes_table (JokeID, Joke_question, Joke_answer, users_id1) VALUES (NULL, ?, ?, ?)");
+$stmt->bind_param("ssi", $new_joke_question, $new_joke_answer,$userid);
 
+echo"<pre>";
+print_r($stmt);
+echo"</pre>";
 
-// echo"<h2>Show all jokes with the word $keywordfromform</h2>";
-$sql = "INSERT INTO jokes_table (JokeID, Joke_question, Joke_answer, users_id1) VALUES (NULL, '$new_joke_question', '$new_joke_answer', '$userid')"; 
-$result = $mysqli->query($sql);
+$result = $stmt->execute();
+echo"<pre>";
+print_r($result);
+echo"</pre>";
+
+$stmt->close();
 
 include "search_all_jokes.php";
 
+echo"<a href = 'index.php'>Return to main</a>";
 
 ?>
 
-<a href = 'index.php'>  Return to main </a>
---
+++ show_keyboards.php
@@ -0,0 +1,10 @@
+<head>
+<style>
+#main_container 
+{
+	margin:20px;
+}
+</style>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
